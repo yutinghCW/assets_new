d48500d013f7929bbf6ba0e2a9a85e02f78bdfa4
jessicaliuCW <33117298+jessicaliuCW@users.noreply.github.com>
2017-10-26 16:45:14 +0800

    event files
    event files updated

---
+++ event/js/event-slide.js
@@ -0,0 +1,14 @@
+$(function () {
+var swiper = new Swiper('.swiper-container', {
+        pagination: '.swiper-pagination',
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        paginationClickable: true,
+        spaceBetween: 0,
+        centeredSlides: true,
+		loop: true,
+        autoplay: 3000,
+        autoplayDisableOnInteraction: false
+    });
+
+});
