80e35f9bbce3b5605793eb4a487ca501b281714a
jessicaliuCW <33117298+jessicaliuCW@users.noreply.github.com>
2017-10-26 16:40:59 +0800

    cw2000 files updated
    cw2000 files

---
+++ cw2000/js/customize.js
@@ -0,0 +1,13 @@
+$(function(){
+	function collapseNavbar() {
+		if ($(".navbar").offset().top > 30) {
+				$(".navbar-fixed-top").addClass("top-nav-collapse");
+		} else {
+				$(".navbar-fixed-top").removeClass("top-nav-collapse");
+		}
+	}
+
+	$(window).scroll(collapseNavbar);
+	$(document).ready(collapseNavbar);
+
+});
